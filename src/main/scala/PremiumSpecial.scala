bcac068649e4c85d8e4c2976eaf83e87956fd56b
AprilGar <april.garlington@hotmail.co.uk>
2025-03-05 14:48:07 +0000

    add Coverage instuctions as mark down

--- src/main/scala/PremiumSpecial.scala
+++ src/main/scala/PremiumSpecial.scala
@@ -1,7 +1,3 @@
 case class PremiumSpecial(name: String, price: BigDecimal) extends MenuItem {
   def itemCategory: ItemCategory = PremiumMeal
 }
-object PremiumSpecial {
-  val specialDrink = PremiumSpecial("Special Drink", 4.25)
-  val specialFood = PremiumSpecial("Special Food", 8.75)
-}
--- src/main/scala/StandardItem.scala
+++ src/main/scala/StandardItem.scala
@@ -1,16 +1,2 @@
-
-
-
 case class StandardItem(name: String, price: BigDecimal, itemCategory: ItemCategory) extends MenuItem
 
-object StandardItem {
-  val coffee = StandardItem("Coffee", 3.25, HotDrink)
-  val tea = StandardItem("Tea", 2.50, HotDrink)
-  val icedCoffee = StandardItem("Iced Coffee", 3.25, ColdDrink)
-  val icedTea = StandardItem("Iced Tea", 2.95, ColdDrink)
-  val fruitJuice = StandardItem("Fruit Juice", 2.95, ColdDrink)
-  val sandwich = StandardItem("Sandwich", 5.95, ColdFood)
-  val cake = StandardItem("Cake", 4.95, ColdFood)
-  val soup = StandardItem("Soup", 7.95, HotFood)
-  val Toastie = StandardItem("Toastie", 7.95, HotFood)
-}
